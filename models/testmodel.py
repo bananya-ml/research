febd586086a1740346067b6847d541dea53f8ea1
Ananya Bhatnagar <bhatnagarananya64@gmail.com>
2024-06-06 16:22:54 +0530

    new test model, add early stopping

--- models/testmodel.py
+++ models/testmodel.py
@@ -1,34 +1,49 @@
 from torch.nn import functional as F
 from torch import nn
+import torch
 from torchsummary import summary
 
 class TestConvModel(nn.Module):
     def __init__(self):
         super(TestConvModel, self).__init__()
+        
+        #self.cnn = nn.Sequential()
         self.conv1 = nn.Conv1d(in_channels=1, out_channels=4, kernel_size=3, padding=1)
         self.conv2 = nn.Conv1d(in_channels=4, out_channels=16, kernel_size=3, padding=1)
         self.pool = nn.MaxPool1d(kernel_size=2)
-        self.fc1 = nn.Linear(16 * (343 // 4), 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, 128)
-        self.fc4 = nn.Linear(128, 64)
-        self.fc5 = nn.Linear(64, 1)
+        self.fc01 = nn.Linear(16 * (343 // 4), 128)
+        self.fc02 = nn.Linear(128, 64)
+        
+        #self.ctx = nn.Sequential()
+        self.fc11 = nn.Linear(2, 16)
+        self.fc12 = nn.Linear(16,32)
 
-    def forward(self, x):
+        self.fc = nn.Linear(64+32, 64)
+        self.output = nn.Linear(64, 1)
+
+
+    def forward(self, x, y):
+
         x = self.pool(F.leaky_relu(self.conv1(x)))
         x = self.pool(F.leaky_relu(self.conv2(x)))
         x = x.view(x.size(0), -1)
-        x = F.leaky_relu(self.fc1(x))
-        x = F.leaky_relu(self.fc2(x))
-        x = F.leaky_relu(self.fc3(x))
-        x = F.leaky_relu(self.fc4(x))
-        x = self.fc5(x)
+        x = F.leaky_relu(self.fc01(x))
+        x = F.leaky_relu(self.fc02(x))
+        
+        y = F.leaky_relu(self.fc11(y))
+        y = F.leaky_relu(self.fc12(y))
 
-        return x
+        out = torch.cat((x, y), 1)
+        out = F.leaky_relu(self.fc(out))
+        out = self.output(out)
+        
+        return out
 
 
 #if __name__ == "__main__":
     #a = torch.rand(32, 1, 343)
+    #b = torch.rand(32, 2)
     #model = TestConvModel()
-    #out = model(a)
+    #out = model(a, b)
+
     #summary(model, (1, 343))
