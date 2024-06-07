b35c118b787c1c0912377a23bd9786d89e6c88cb
Ananya Bhatnagar <bhatnagarananya64@gmail.com>
2024-06-07 20:53:05 +0530

    add predict.ipynb and trained model

--- models/testmodel.py
+++ models/testmodel.py
@@ -7,30 +7,34 @@ class TestConvModel(nn.Module):
     def __init__(self):
         super(TestConvModel, self).__init__()
         
-        #self.cnn = nn.Sequential()
+        # spectrum convolutional input
         self.conv1 = nn.Conv1d(in_channels=1, out_channels=4, kernel_size=3, padding=1)
         self.conv2 = nn.Conv1d(in_channels=4, out_channels=16, kernel_size=3, padding=1)
         self.pool = nn.MaxPool1d(kernel_size=2)
         self.fc01 = nn.Linear(16 * (343 // 4), 128)
         self.fc02 = nn.Linear(128, 64)
         
-        #self.ctx = nn.Sequential()
+        # context input
         self.fc11 = nn.Linear(2, 16)
-        self.fc12 = nn.Linear(16,32)
+        self.bn1 = nn.BatchNorm1d(16)
+        self.fc12 = nn.Linear(16, 32)
 
+        # combine outputs
         self.fc = nn.Linear(64+32, 64)
         self.output = nn.Linear(64, 1)
 
 
     def forward(self, x, y):
 
+        # forward pass for spectrum
         x = self.pool(F.leaky_relu(self.conv1(x)))
         x = self.pool(F.leaky_relu(self.conv2(x)))
         x = x.view(x.size(0), -1)
         x = F.leaky_relu(self.fc01(x))
         x = F.leaky_relu(self.fc02(x))
         
-        y = F.leaky_relu(self.fc11(y))
+        # forward pass for context
+        y = self.bn1(F.leaky_relu(self.fc11(y)))
         y = F.leaky_relu(self.fc12(y))
 
         out = torch.cat((x, y), 1)
@@ -39,11 +43,10 @@ class TestConvModel(nn.Module):
         
         return out
 
-
 #if __name__ == "__main__":
     #a = torch.rand(32, 1, 343)
     #b = torch.rand(32, 2)
     #model = TestConvModel()
     #out = model(a, b)
 
-    #summary(model, (1, 343))
+    #summary(model)
